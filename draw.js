bcd3856ac7ea3e6f8d088ceb0047b141100a9aec
ganduro <ganduro@gmail.com>
2015-01-29 13:41:57 -0800

    Added project files

---
+++ draw.js
@@ -0,0 +1,18 @@
+// state variables, helper functions, etc.
+// go here...
+
+
+// one-time initialization.
+// by default, this method is only invoked once, upon page launch.
+// - canvas: an HTML canvas element
+// - ctx: a 2D drawing context for the canvas
+function init(canvas, ctx) {
+}
+
+
+// frame drawing routine.
+// this method will be invoked once for every frame.
+// - canvas: an HTML canvas element
+// - ctx: a 2D drawing context for the canvas
+function draw(canvas, ctx) {
+}
