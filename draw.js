4853043bed4e501d890069aa4eb952fd7d3442f7
ganduro <ganduro@gmail.com>
2015-02-05 14:34:10 -0800

    Made the o'clock stuff work, sort of
    They appear and orbit, just…not where you click

--- draw.js
+++ draw.js
@@ -19,26 +19,6 @@ var frameCount = 0;
 	var planetThreeG = Math.floor(Math.random()*255);
 	var planetThreeB = Math.floor(Math.random()*255);
 
-//defines 
-function addPlanet(x, y, radius, r, g, b) {
-  planets.push({
-    x: x,
-    y: y,
-    radius: radius,
-    c: "rgb(" + r + "," + g + "," + b + ")"
-  });
-}
-
-// add a new planet
-function onAddEvent(evt) {
-  // randomize color choices
-  var r = Math.floor(Math.random()*255);
-  var g = Math.floor(Math.random()*255);
-  var b = Math.floor(Math.random()*255);
-  // add the planet at the mouse offsetX and offsetY
-  addPlanet(evt.offsetX, evt.offsetY, r, g, b);
-}
-
 // one-time initialization.
 // by default, this method is only invoked once, upon page launch.
 // - canvas: an HTML canvas element
@@ -50,22 +30,43 @@ function init(canvas, ctx) {
 	canvas.addEventListener("click", onAddEvent);
 }
 
-function getPolar(x, y) {
-  var radius = Math.sqrt(x*x + y*y);
-  var angle = Math.atan2(y, x);
-  return [radius, angle];
+function onAddEvent(event) {
+	var x = event.offsetX;
+	var y = event.offsetY;
+	var p = createPlanet(x, y);
+	planets.push(p);
 }
 
-function drawPlanet(p, ctx) {
+function createPlanet(x, y) {
+	var p = {
+		r: Math.floor(Math.random()*255),
+		g: Math.floor(Math.random()*255),
+		b: Math.floor(Math.random()*255),
+		distance: Math.sqrt(x*x + y*y),
+		angle: Math.atan2(y, x),
+		radius: 15 + Math.floor(30*Math.random()),
+		angularSpeed: 0.5 + Math.random()*3,
+		dir: Math.random() < 0.5 ? -1 : 1
+	};
+	return p;
+}
+
+function drawPlanet(planet, ctx) {
+
   // update planet angle
-  p.angle += p.angularSpeed;
+  planet.angle += planet.angularSpeed;
 
   // dir is either 1 or -1
-  var x = p.distance * p.dir * Math.cos(p.angle * Math.PI/180);
-  var y = p.distance * p.dir * Math.sin(p.angle * Math.PI/180);
+  var x = planet.distance * planet.dir * Math.cos(planet.angle * Math.PI/180);
+  var y = planet.distance * planet.dir * Math.sin(planet.angle * Math.PI/180);
 
-  // draw planet at location x, y
 
+  // draw planet at location x, y with radius and color randomized
+  	ctx.beginPath();
+  	ctx.fillStyle = "rgb(" + planet.r + "," + planet.g + "," + planet.b + ")";
+	ctx.arc(x,y,planet.radius,0,2*Math.PI);
+	ctx.fill();
+	ctx.closePath();
 }
 
 // frame drawing routine.
@@ -73,8 +74,13 @@ function drawPlanet(p, ctx) {
 // - canvas: an HTML canvas element
 // - ctx: a 2D drawing context for the canvas
 function draw(canvas, ctx) {
+
 	ctx.fillStyle = "black";
-  ctx.fillRect(-w, -h, 2*w, 2*h);
+ 	ctx.fillRect(-w, -h, 2*w, 2*h);
+
+	for (var i=0; i<planets.length; ++i) {
+		drawPlanet(planets[i],ctx);
+	}
 
   	ctx.beginPath();
   	ctx.fillStyle = 'gold';
@@ -83,11 +89,11 @@ function draw(canvas, ctx) {
 	ctx.closePath();
 
   	if (frameCount % wait === 0) {
-    // rotate all the things.
-    ctx.rotate(angle * Math.PI/180);
+    // DON'T ROTATE ALL THE THINGS
+    //ctx.rotate(angle * Math.PI/180);
 
     //make some planets, son
-  	ctx.beginPath();
+  	/*ctx.beginPath();
   	ctx.fillStyle = "rgb(" + planetOneR + "," + planetOneG + "," + planetOneB + ")";
 	ctx.arc(-100,-100,30,0,2*Math.PI);
 	ctx.fill();
@@ -103,7 +109,7 @@ function draw(canvas, ctx) {
   	ctx.fillStyle = "rgb(" + planetThreeR + "," + planetThreeG + "," + planetThreeB + ")";
 	ctx.arc(-100,150,40,0,2*Math.PI);
 	ctx.fill();
-	ctx.closePath();
+	ctx.closePath();*/
     
   }
   // update the frame count
